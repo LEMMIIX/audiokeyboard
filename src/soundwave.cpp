bd8e6dd33316229affd5868bbb7dbc0e29d23418
LEMMIIX <lemmiix@gmail.com>
2025-04-17 15:12:40 +0200

    erzeugen eines simplen Sinus-Tons beim Öffnen des Programms

--- src/soundwave.cpp
+++ src/soundwave.cpp
@@ -1,2 +1,27 @@
+#include <cmath>
+
 #include "soundwave.h"
 
+unsigned int sampleRate = 44100;
+unsigned int bufferFrames = 256;
+double phase = 0.0;
+const double FREQUENCY = 440.0;
+
+int single_tone(void* o_buff, void* i_buff, unsigned int nBufferFrames,
+                double streamTime, RtAudioStreamStatus status, void* userData) {
+
+    std::cout << "single_tone spielt gerade einen hübschen Ton ja ja" << std::endl;
+    double* buffer = (double*) o_buff;
+
+    for(unsigned int i = 0; i < nBufferFrames; ++i) {
+        *buffer++ = 0.5 * sin(phase);
+        *buffer++ = 0.5 * sin(phase);
+
+        phase += (2.0 * M_PI) * FREQUENCY / sampleRate;
+        if(phase >= (2.0 * M_PI)) {
+            phase -= (2.0 * M_PI);
+        }
+    }
+
+    return 0;
+}
--- src/soundwave.h
+++ src/soundwave.h
@@ -3,5 +3,14 @@
 
 #include "../lib/rtaudio/RtAudio.h"
 
+extern unsigned int sampleRate;
+extern unsigned int bufferFrames;
+extern double phase;
+extern const double FREQUENCY;
+
+static RtAudio::StreamParameters o_params;
+
+int single_tone(void* o_buff, void* i_buff, unsigned int nBufferFrames,
+                double streamTime, RtAudioStreamStatus status, void* userData);
 
 #endif // SOUNDWAVE_H
