41a07defab36b4fec61bdeaec0d9e1e662ac21e6
LEMMIIX <lemmiix@gmail.com>
2025-04-28 15:21:16 +0200

    verlagert alte header Variablen in toneengine

--- src/ToneInfo.h
+++ src/ToneInfo.h
@@ -5,7 +5,10 @@ struct ToneInfo {
     bool active;
     double frequency;
     double phase;
+    // Attack-Decay-Sustain-Release (ADSR) Hülle
     double attack;	// Tonlautstärke stiegt an
+    double decay;
+    double sustain;
     double release;	// Tonlautstärke nimmt ab
 };
 
